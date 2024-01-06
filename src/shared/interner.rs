dee83acf451a210a56a39204d754cfcb48826ba2
Im-Beast <franik.mateusz@gmail.com>
2024-01-06 17:03:37 +0100

    feat: convert `&str` to `String` to remove lifetime restrictions

--- src/shared/interner.rs
+++ src/shared/interner.rs
@@ -3,12 +3,12 @@ use std::collections::HashMap;
 pub type StringID = usize;
 
 #[derive(Debug, Clone)]
-pub struct Interner<'a> {
-    map: HashMap<&'a str, StringID>,
+pub struct Interner {
+    map: HashMap<String, StringID>,
     strings: Vec<String>,
 }
 
-impl Default for Interner<'_> {
+impl Default for Interner {
     fn default() -> Self {
         Self {
             map: HashMap::new(),
@@ -17,14 +17,25 @@ impl Default for Interner<'_> {
     }
 }
 
-impl<'a> Interner<'a> {
-    pub fn intern(&mut self, name: &'a str) -> StringID {
+impl Interner {
+    pub fn intern(&mut self, name: &str) -> StringID {
         if let Some(id) = self.map.get(name) {
             return *id;
         }
 
         let id = self.strings.len();
+        self.map.insert(name.to_string(), id);
         self.strings.push(name.to_string());
+        id
+    }
+
+    pub fn intern_string(&mut self, name: String) -> StringID {
+        if let Some(id) = self.map.get(&*name) {
+            return *id;
+        }
+
+        let id = self.strings.len();
+        self.strings.push(name.to_owned());
         self.map.insert(name, id);
         id
     }
